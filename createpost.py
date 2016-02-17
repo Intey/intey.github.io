745ecedb13f940753d27ce7ce5e3919fa036ded3
Intey <ziexe0@gmail.com>
2016-02-17 23:46:33 +0300

    [up][scripts] Create post and open in vim.

--- createpost.py
+++ createpost.py
@@ -4,15 +4,33 @@ from subprocess import call
 from datetime import datetime as d
 import sys
 
+
+def write_to_file(path, lines):
+    file = open(path, 'w', encoding="UTF8")
+    file.writelines(lines)
+    file.close()
+
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Supply name of post")
         exit(1)
 
     post = sys.argv[-1]
-    now = d.now()
 
-    call(["touch", "_posts/{0}-{1}-{2}-{3}.markdown".format(now.year,
-                                                            now.month,
-                                                            now.day,
-                                                            post)])
+    now = d.now()
+    date_str = "{0}-{1}-{2}".format(now.year, now.month, now.day)
+
+    lines = [
+        "---\n",
+        "layout: post\n",
+        "title: \"%s\"\n" % post,
+        "date: %s\n" % date_str,
+        "categories: experimental\n",
+        "---\n"
+    ]
+
+    postname = "_posts/{0}-{1}.markdown".format(date_str, post)
+
+    call(["touch", postname])
+    write_to_file(postname, lines)
+    call(["gvim", postname])
